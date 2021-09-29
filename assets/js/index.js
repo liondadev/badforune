88908ef8e1d3c1dcd1e1b7fcf1b44200aafa5e8a
Phix - Laptop <phixmeplease@gmail.com>
2021-09-29 10:59:40 -0400

    very needed fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,14 @@ const fortunes = [
     "You will <strong>die alone</strong>.",
     "You will <strong>combust randomly</strong> at the age of <strong>23</strong>",
     "Noone will marry you because you <strong>smell terrible</strong>.",
-    "Error 404: Fortune not found. If you belive this is an error please contact the site administrator.",
-    "Rizaelning you can raed this mepsislled fnurrtone will be the hgihgliht of yuor day.",
-    "Pigeon poop burns the retina for 13 hours. You will learn this the hard way.",
-    "You will get pooped on by a cow then fall over and die",
-    "You will get cancelled by twitter",
+    "<strong>Error 404</strong>: Fortune not found. If you belive this is an error please contact the site administrator.",
+    "Rizaelning you can raed this <strong>mepsislled fnurrtone</strong> will be the hgihgliht of yuor day.",
+    "<strong>Pigeon poop burns the retina</strong> for 13 hours. You will <strong>learn this the hard way</strong>.",
+    "You will get <strong>pooped on by a cow</strong> then <strong>fall over</strong> and <strong>die</strong>",
+    "You will get <strong>cancelled on twitter</strong>",
+    "You will <strong>drink</strong> too much <strong>gasoline</strong>",
+    "Your <strong>dad will go to the store</strong> to get milk and <strong>never come back</strong>",
+    "You will <strong>loose your minecraft bedwars winstreak</strong> of <strong>420</strong>",
 ];
 var lastFortune = "";
 
